b10a8f3aac1db57cb31898cf9615f1fa32de0e87
zsluder <zsluder@omnispear.com>
2017-08-16 17:33:17 -0400

    Cleanup

--- Laravel-Setup.ps1
+++ Laravel-Setup.ps1
@@ -3,8 +3,7 @@
 #          Laravel 5.4 Windows Setup
 # --------------------------------------------
 
-
-# ----- SETUP 
+# Pre-setup 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $wsh = New-Object -ComObject WScript.Shell
 Write-Host " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -12,7 +11,7 @@ Write-Host "|`t Laravel 5.4 Installation `t|"
 Write-Host " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`n"
 
 
-# ----- DOWNLOAD VIRTUALBOX v5.1.22
+# Download and install Virtualbox v5.1.22
 Write-Host "Downloading VirtualBox v5.1.22 ..."
 Invoke-WebRequest http://download.virtualbox.org/virtualbox/5.1.22/VirtualBox-5.1.22-115126-Win.exe -OutFile $PSScriptRoot\VirtualBox-Install.exe
 Start-Process -FilePath "$PSScriptRoot\VirtualBox-Install.exe" -wait
@@ -20,7 +19,7 @@ Write-Host "`t- Finished"
 Remove-Item -path $PSScriptRoot\VirtualBox-Install.exe
 
 
-# ----- DOWNLOAD VAGRANT v1.9.5
+# Download and install Vagrant v1.9.5
 Write-Host "Downloading Vagrant v1.9.5 ..."
 Invoke-WebRequest https://releases.hashicorp.com/vagrant/1.9.5/vagrant_1.9.5.msi -OutFile $PSScriptRoot\Vagrant-Install.msi
 Start-Process -FilePath "$PSScriptRoot\Vagrant-Install.msi" -wait
@@ -28,14 +27,14 @@ Write-Host "`t- Finished"
 Remove-Item -path $PSScriptRoot\Vagrant-Install.msi
 
 
-# ----- DOWNLOAD GIT SHELL v2.12.0
+# Download and install Git v2.12.0
 Write-Host "Downloading Git Shell v2.12.0 ..."
 Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.13.0.windows.1/Git-2.13.0-32-bit.exe -OutFile $PSScriptRoot\Git-Install.exe
 Start-Process -FilePath "$PSScriptRoot\Git-Install.exe" -wait
 Write-Host "`t- Finished"
 Remove-Item -path $PSScriptRoot\Git-Install.exe
 
-# ----- SET UP DEVELOPMENT ENVIRONMENT
+# Set up development environment 
 $email = Read-Host -Prompt 'Enter Omnispear email: '
 $wsh.SendKeys('{ENTER}')
 $wsh.SendKeys('{ENTER}')
@@ -66,7 +65,7 @@ vagrant box add laravel/homestead
 vagrant up
 vagrant ssh
 
-Write-Host "`nThe script has finished seting up"
+Write-Host "`nThe script has finished setting up"
 
 
 # Network drivers : C:\Program Files\Oracle\VirtualBox\drivers\network install both .inf
